1c853543284a539dfbfb6017794c83c91eba1e24
Teekeks <info@teawork.de>
2024-05-14 09:36:01 +0200

    feat: allow for different time each day and allow customized user list

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import schedule
 import json
 from os.path import exists
+from functools import partial
 
 if not exists('config.json'):
     print('Please create a config.json file.')
@@ -13,21 +14,29 @@ with open('config.json', 'r') as f:
     cfg = json.load(f)
 
 
-def post_message():
-    user_list = random.sample(cfg['users'], len(cfg['users']))
+def post_message(_day):
+    users = [u for u in cfg['users'] if u not in _day.get('excluded', [])]
     requests.post('https://slack.com/api/chat.postMessage', {
         'token': cfg['bot_token'],
         'channel': cfg['channel'],
-        'text': ', '.join(user_list)
+        'text': ', '.join(random.sample(users, len(users)))
     })
 
 
-# schedule every work day
-schedule.every().monday.at(cfg['time_of_day']).do(post_message)
-schedule.every().tuesday.at(cfg['time_of_day']).do(post_message)
-schedule.every().wednesday.at(cfg['time_of_day']).do(post_message)
-schedule.every().thursday.at(cfg['time_of_day']).do(post_message)
-schedule.every().friday.at(cfg['time_of_day']).do(post_message)
+day_lookup = {
+    'mon': schedule.every().monday,
+    'tue': schedule.every().tuesday,
+    'wed': schedule.every().wednesday,
+    'thu': schedule.every().thursday,
+    'fri': schedule.every().friday,
+    'sat': schedule.every().saturday,
+    'sun': schedule.every().sunday,
+}
+
+# create schedule from config
+for day, schedule in cfg['schedules']:
+    print(f'scheduled posting for {day} at {schedule["time_of_day"]}')
+    day_lookup[day].at(schedule['time_of_day']).do(partial(post_message, schedule))
 
 while True:
     schedule.run_pending()
